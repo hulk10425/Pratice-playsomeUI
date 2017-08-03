3ed496be294dd69ac813b0c7b31ec07341d72852
hulk10425 <hulk104255@gmail.com>
2017-08-03 09:48:33 +0800

    start webview

---
+++ UI/UI/WebViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WebViewController.swift
+//  UI
+//
+//  Created by 陳遵丞 on 2017/8/3.
+//  Copyright © 2017年 hulk. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+
+class WebViewController: UIViewController {
+
+    var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
